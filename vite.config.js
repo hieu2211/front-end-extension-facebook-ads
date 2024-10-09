8e257cc3de7f05251f7dd3f04fa2807030b6cb95
hieu2211 <43336147+hieu2211@users.noreply.github.com>
2024-10-09 09:57:21 +0000

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/", // Đường dẫn gốc cho ứng dụng
-  build: {
-    outDir: "dist", // Đường dẫn thư mục đầu ra
-    assetsDir: "static", // Đổi tên thư mục assets thành static
-  },
   plugins: [react()],
   server: {
     host: "0.0.0.0",
